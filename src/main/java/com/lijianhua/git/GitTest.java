2d6db51ac012e1a5ba01789751cc7c14cdb493a9
午夜.bicycle <1976288368@qq.com>
2022-09-25 15:12:04 +0800

    first commit

---
+++ src/main/java/com/lijianhua/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.lijianhua.git;
+
+/**
+ * @author 李建华
+ * @create 2022/9/25 15:07
+ * @description 自己写
+ * @since 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
