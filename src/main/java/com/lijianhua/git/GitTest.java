370f15addea4479eb796a12b34860265896f24bb
wind <75659710+lijianhua-wind@users.noreply.github.com>
2022-09-25 16:18:11 +0800

    pull test

--- src/main/java/com/lijianhua/git/GitTest.java
+++ src/main/java/com/lijianhua/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hello hot fix2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
