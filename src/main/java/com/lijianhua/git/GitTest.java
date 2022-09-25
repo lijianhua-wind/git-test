402b34c14e171bd7e0165c05865d1a2044f30f39
午夜.bicycle <1976288368@qq.com>
2022-09-25 16:10:02 +0800

    push commit

--- src/main/java/com/lijianhua/git/GitTest.java
+++ src/main/java/com/lijianhua/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello hot fix");
         System.out.println("master test");
         System.out.println("hello hot fix2");
+        System.out.println("push test");
     }
 }
